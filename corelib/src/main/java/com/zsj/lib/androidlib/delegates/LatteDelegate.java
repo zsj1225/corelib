fbfb404f249fc9aff5f5259a888ba1068e2b3d29
zhushengjun <1185335441@qq.com>
2017-10-15 23:31:03 +0800

    单Activity框架+2

--- corelib/src/main/java/com/zsj/lib/androidlib/delegates/LatteDelegate.java
+++ corelib/src/main/java/com/zsj/lib/androidlib/delegates/LatteDelegate.java
@@ -5,9 +5,4 @@ package com.zsj.lib.androidlib.delegates;
  */
 
 public abstract class LatteDelegate extends PermissionCheckerDelegate {
-
-    @SuppressWarnings("unchecked")
-    public <T extends LatteDelegate> T getParentDelegate() {
-        return (T) getParentFragment();
-    }
 }
